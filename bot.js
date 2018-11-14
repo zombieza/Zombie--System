e2378074d058022a33c000817852daa0ea9062e4
zombieza <44494625+zombieza@users.noreply.github.com>
2018-11-14 22:02:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,11 +69,7 @@ client.on("guildMemberAdd", member => {
          moment.locale('ar-ly');
          var h = member.user;
         let heroo = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(h.avatarURL)
-        .setAuthor(h.username,h.avatarURL)
         .addField(': تاريخ دخولك الدسكورد',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)                  
-         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
      welcomer.send({embed:heroo});          
          
       var Canvas = require('canvas')
