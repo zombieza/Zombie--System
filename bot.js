3b720d59dbc52fc298b7dca7e22699d970d63229
zombieza <44494625+zombieza@users.noreply.github.com>
2018-11-19 13:25:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -877,6 +877,37 @@ message.guild.member(user).kick();
 });
 
 
+let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
+let banse = new Set();
+client.on('guildBanAdd', function(guild) {
+  guild.fetchAuditLogs().then(logs => {
+    const ser = logs.entries.first().executor;
+    if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
+      bans: 3
+    }
+    let boner = bane[ser.id+guild.id]
+banse.add(ser.id)
+boner.bans = Math.floor(boner.bans+2)
+
+
+setTimeout(() => {
+  boner.bans = 3
+  banse.delete(ser.id)
+},8000)
+
+if(boner.bans > 3 {
+  let roles = guild.members.get(ser.id).roles.array()
+guild.members.get(ser.id).removeRoles(roles)
+}
+
+    })
+    fs.writeFile('./bcer.json', JSON.stringify(bane), (err) => {
+if (err) console.error(err);
+})
+
+})
+
+
 
 //MHSTR END NOW THIS IS END
 client.login(process.env.BOT_TOKEN);
